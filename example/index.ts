2714146605783c23b4ee19b8badcc78f92738931
nichoth <nichoth@nichoth.com>
2025-12-17 15:55:38 -0800

    fix example

--- example/index.ts
+++ example/index.ts
@@ -1,11 +1,17 @@
 import { type FunctionComponent, render } from 'preact'
 import { html } from 'htm/preact'
-import { example } from '../src/index.js'
+import { MetaImage } from '../src/index.js'
 
-example()
+const imageSrc = MetaImage({
+    cloudName: 'dhv2youtt',
+    filename: 'cube_vufm7u.png',
+    text: 'hello images'
+})
 
 const Example:FunctionComponent<unknown> = function () {
-    return html`<div>hello</div>`
+    return html`<div>
+        <img src=${imageSrc} />
+    </div>`
 }
 
 render(html`<${Example} />`, document.getElementById('root')!)
